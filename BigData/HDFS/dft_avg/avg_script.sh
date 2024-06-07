cc5bc39376cf180f7bc3b8192a836cc8c717df08
Papon_C <61746127+Canppchk@users.noreply.github.com>
2024-06-07 15:32:23 +0900

    Delete BigData directory

--- BigData/HDFS/dft_avg/avg_script.sh
+++
@@ -1,9 +0,0 @@
-rm -r ./averageprice_classes
-mkdir averageprice_classes
-rm ac.jar
-javac -d averageprice_classes AveragePrice.java
-jar cvf ac.jar -C  averageprice_classes/ .
-hdfs dfs -rm -r /dft-output2
-hadoop jar ac.jar AveragePrice /dft  /dft-output2
-
-hdfs dfs -cat /dft-output2/part-r-00000
